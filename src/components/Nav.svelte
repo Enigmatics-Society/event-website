ee53f1cd7af03af8d2d3bf350d481b5c699fcc39
therealansh <tyagiansh23@hotmail.com>
2020-10-01 20:57:18 +0530

    init homepage

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,216 @@
 <script>
-	export let segment;
+	let active = false;
+  let addActive = "";
+
+  let isOpen = false;
+
+  function handleClick() {
+    isOpen = !isOpen;
+  }
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
+	.nav-head {
+    max-height: 90px;
+    width: auto;
+  }
+  nav {
+    font-weight: 300;
+    padding: 1em 2em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 10vh;
+  }
 	ul {
-		margin: 0;
-		padding: 0;
-	}
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  li {
+    list-style: none;
+  }
 
-	li {
-		display: block;
-		float: left;
-	}
+  li:not(:last-child) {
+    margin-right: 50px;
+  }
 
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
+  .nav-link {
+    text-decoration: none;
+    padding: 0.1em 0.25em;
+    display: block;
+    text-transform: uppercase;
+    font-size: 1.6rem;
+    transition: all 0.5s;
+    position: relative;
+    border-bottom: 2px solid #000;
+  }
 
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
+  .nav-link::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    z-index: -1;
+    height: 0;
+    opacity: 0;
+    transition: all 0.6s;
+    background-color: #df85d9;
+  }
 
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+  .nav-link:hover::after {
+    height: 100%;
+    opacity: 1;
+  }
+
+  .nav-link:hover {
+    color: #fff;
+  }
+
+  .burger-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    transition: all 0.5s ease-in-out;
+    display: none;
+    z-index: 10;
+  }
+
+  .hamburger-icon {
+    width: 30px;
+    height: 2px;
+    background: #000;
+    border-radius: 5px;
+    transition: all 0.5s ease-in-out;
+  }
+
+  .hamburger-icon::before,
+  .hamburger-icon::after {
+    content: "";
+    position: absolute;
+    width: 30px;
+    height: 2px;
+    background: #000;
+    border-radius: 5px;
+    transition: all 0.5s ease-in-out;
+  }
+
+  .hamburger-icon::before {
+    transform: translateY(-11px);
+  }
+
+  .hamburger-icon::after {
+    transform: translateY(11px);
+  }
+
+  .burger-container.open .hamburger-icon {
+    transform: translateX(-50px);
+    background: transparent;
+  }
+
+  .burger-container.open .hamburger-icon::before {
+    transform: rotate(45deg) translate(35px, -35px);
+  }
+
+  .burger-container.open .hamburger-icon::after {
+    transform: rotate(-45deg) translate(35px, 35px);
+  }
+
+  .nav-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background: #000;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    overflow-x: hidden;
+    transform: translateX(100%);
+    transition: all 0.6s ease-in-out;
+    z-index: 5;
+  }
+
+  .nav-overlay.open {
+    transform: translateX(0%);
+  }
+
+  .nav-overlay-list {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+  }
+
+  .nav-overlay-list li {
+    position: relative;
+    list-style: none;
+    text-align: center;
+    display: block;
+  }
+
+  .nav-overlay-list li a {
+    position: relative;
+    text-decoration: none;
+    font-size: 1.4em;
+    padding: 0 10px;
+    color: #000;
+    font-weight: 700;
+    text-transform: uppercase;
+    display: inline-block;
+    margin-top: 40px;
+    border: none;
+    margin-bottom: 30px;
+  }
+
+  .divider-big {
+    height: 1px;
+    width: 70%;
+    margin: 0 auto;
+  }
+	@media (max-width: 768px) {
+    .nav-links {
+      display: none;
+    }
+
+    .nav-head {
+      max-height: 70px;
+    }
+
+    li:not(:last-child) {
+      margin-right: 0;
+      margin-bottom: 1em;
+    }
+
+    .burger-container {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 </style>
 
 <nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+	<a href=".">
+		<img src = "collab.png" alt="header" class="nav-head"/>
+	</a>
+	<div class="burger-container" on:click={handleClick} class:open={isOpen}>
+		<div class="hamburger-icon"></div>
+	</div>
+	<ul class="nav-links ">
+		<li><a class="nav-link" href=".">home</a></li>
+		<li><a class="nav-link" href="events">events</a></li>
+		<li><a rel="prefetch" class="nav-link" href="team">team</a></li>
+	  </ul>
+	  <div class="nav-overlay" on:click={handleClick} class:open={isOpen}>
+	  </div>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,109 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+.logo-container {
+    text-align: center;
+    width: 850px;
+    height: 400px;
+    font-size: 2rem;
+    position: relative;
+    transform-style: preserve-3d;
+    transition: .4s;
+    perspective: 800px;
+  }
+  .logo-front{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    transform: rotateX(0deg);
+    transition: .4s;
+  }
+  .logo-container:hover .logo-front{
+    transform: rotateX(-180deg);
+  }
+  .about-back{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    font-size: 1.1rem;
+    backface-visibility: hidden;
+    transform: rotateX(180deg);
+    transition: .4s;
+  }
+  .about-back > p{
+    line-height: 1.9;
+    word-spacing: 1.5;
+  }
+  .about-back > h2{
+    background-image: linear-gradient(to right,#ffc7d1, #5a03cc);
+    -webkit-background-clip: text;
+    color: transparent;
+    font-weight: 700;
+    margin: 5px;
+  }  
+  .divider{
+    height: 3px;
+    width: 140px;
+    background-image: linear-gradient(to right, #ffc7d1, #5a03cc);
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -30%);
+  }
+  .logo-container:hover .about-back{
+    transform: rotateX(0deg);
+  }
+	  img {
+    width: 100%;
+    max-width: 600px;
+    margin: 0 0 1em 0;
+  }
+  a {
+    outline: none;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+.logo-front {
+      font-size: 1.5rem;
+    }
+  @media (max-width: 500px) {
+    .logo-container {
+      display: inline-block;
+      width: 300px;
+      height: 160px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    .logo-front > p{
+      font-size: 1.1rem;
+    }
+    .about-back{
+      display: none;
+    }
+    .logo-container:hover .logo-front{
+      transform: none;
+    }
+    #social {
+      
+      margin-top: 110px;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>IIIT Sonepat Hacktober Fest Meetup</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="logo-container">
+	<div class="logo-front">
+		<img alt="logo" src="logo.png" >
+	</div>
+	<div class="about-back">
+		<h2> About </h2>
+		<div class="divider"></div>
+		<p>Test</p>	
+	</div>
+</div>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
