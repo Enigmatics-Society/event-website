083298f09a32209d6ba9d3ad98647bc0c8321c35
therealansh <tyagiansh23@hotmail.com>
2020-10-03 02:38:54 +0530

    added team page

---
+++ src/components/TeamComponent.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let imgSrc;
+  </script>
+  
+  <style>
+    .card {
+      margin-top: 4rem;
+      text-align: center;
+      font-family: "Roboto Mono", sans-serif;
+    }
+  
+    img {
+      width: 100%;
+    }
+
+  </style>
+  
+  <div
+    class="card"
+    data-aos="fade-up"
+    data-aos-once="true"
+    data-aos-duration="1000">
+      <img alt="author-avatar" data-src={imgSrc} class="lazyload" />
+  </div>
+  
