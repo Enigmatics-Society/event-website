7f1384faf340ad4f216611b7b9e443fa498e9bdb
Chandra Mauli Sharma <75089529+Chandra-Mauli-Sharma@users.noreply.github.com>
2021-10-16 14:59:50 +0530

    Added Flip card

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -61,66 +61,81 @@
       font-weight: bold;
       text-align: center;
   }
-
   .card {
     box-shadow: 0 4px 8px 0 rgba(255, 132, 50, 0.568);
     transition: 1s;
     width: auto;
     border-radius: 20px;
   }
-
-  .card:hover {
+  .card:hover .container{
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    transform: rotateY(180deg);
   }
-
   .container {
-    padding: 1 1;
+    position: relative;
+    transition: all 0.75s ease;
+    transform-style: preserve-3d;
+  }
+  .card-front{
+    box-shadow: 0 4px 8px 0 rgba(255, 132, 50, 0.568);
+    border-radius: 20px;
+    padding:3% 3%;
+    margin: auto;
+  }
+  .card-front, .card-back {
+    position: absolute;
+    width: auto;
+    height: auto;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+  .card-back {
     background-color: #f74700;
     border-radius: 20px;
-    padding:5% 5%; 
+    padding:3% 3%; 
+    transform: rotateY(180deg);
+    margin: auto;
   }
 </style>
-
-<div>
-  <img src="{bgImage}" alt="">
-</div>
-<h4 class="font-bold text-lgreen pb-2 text-xs md:text-base pt-5">Presented by</h4>
-<div class="flex space-x-3 md:space-x-10 justify-center items-center">
-  <a target="_blank" rel="noopener" href="https://www.digitalocean.com">
-    <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-digitalocean-gr.f6faef2.svg" alt="DigitalOcean logo" class="h-6 md:h-12">
-  </a> 
-  <a target="_blank" rel="noopener" href="https://hacktoberfest.appwrite.io/">
-    <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-appwrite-gr.1161116.svg" alt="Appwrite logo" class="h-6 md:h-12">
-  </a> 
-  <a target="_blank" rel="noopener" href="http://devmesh.intel.com/">
-    <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-intel-gr.a510816.svg" alt="Intel logo" class="h-5 md:h-10">
-  </a>
-  <a target="_blank" rel="noopener" href="https://deepsource.io/hacktoberfest/">
-    <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-deepsource-gr.37aa9bc.svg" alt="Deepsource logo" class="h-6 md:h-12">
-  </a>
-</div>
 <br>
-<br>
-
 <div class="card">
-
   <div class="container">
-  <h1> About </h1>
-  <br>
-  <p>
-    Welcome to the HacktoberFest event organised by IIIT Sonepat.
-    <br>
-    As our society grows more and more dependent on computers, the ownership of the software we run needs to be more and more decentralised and open sourced.
-    <br>
-    Whether you are a big contributor or a first time programmer, you are welcome to join this event. Learning some and sharing some is key.
-    <br>
-    From getting started with open source to making your first PR, we will be covering it all.
-    <br>
-    A special thanks to Digital Ocean for their immense support</p>	
-     <br>
-    <p><a href="##">Register Here</a></p>
-<br>	
-<p><a href = "##"> Youtube Live Link </a></p>
-
-</div>
+    <div class="card-front">
+      <div>
+        <img src="{bgImage}" alt="">
+      </div>
+      <h4 class="font-bold text-lgreen pb-2 text-xs md:text-base pt-5">Presented by</h4>
+      <div class="flex space-x-3 md:space-x-10 justify-center items-center">
+        <a target="_blank" rel="noopener" href="https://www.digitalocean.com">
+          <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-digitalocean-gr.f6faef2.svg" alt="DigitalOcean logo" class="h-6 md:h-12">
+        </a> 
+        <a target="_blank" rel="noopener" href="https://hacktoberfest.appwrite.io/">
+          <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-appwrite-gr.1161116.svg" alt="Appwrite logo" class="h-6 md:h-12">
+        </a> 
+        <a target="_blank" rel="noopener" href="http://devmesh.intel.com/">
+          <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-intel-gr.a510816.svg" alt="Intel logo" class="h-5 md:h-10">
+        </a>
+        <a target="_blank" rel="noopener" href="https://deepsource.io/hacktoberfest/">
+          <img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-deepsource-gr.37aa9bc.svg" alt="Deepsource logo" class="h-6 md:h-12">
+        </a>
+      </div>
+    </div>
+    <div class="card-back">
+      <h1> About </h1>
+      <br>
+      <p>
+        Welcome to the HacktoberFest event organised by IIIT Sonepat.
+        <br>
+        As our society grows more and more dependent on computers, the ownership of the software we run needs to be more and more decentralised and open sourced.
+        <br>
+        Whether you are a big contributor or a first time programmer, you are welcome to join this event. Learning some and sharing some is key.
+        <br>
+        From getting started with open source to making your first PR, we will be covering it all.
+        <br>
+        A special thanks to Digital Ocean for their immense support</p>	
+         <br>
+        <p><a href="##">Register Here</a></p>	
+        <p><a href = "##"> Youtube Live Link </a></p>
+    </div>
+  </div>
 </div>
